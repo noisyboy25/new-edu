46d8a090e31c648f587e7b9c01b8e075dae4a4d6
noisyboy25 <dj402and@gmail.com>
2021-11-09 18:10:09 +0300

    Fix Heroku port

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static('./client/build'));
 
 app.get('/1', (req, res) => {
   res.end('<h1>Welcome to the app!</h1>');
 });
 
-app.listen(5000);
+app.listen(PORT);
